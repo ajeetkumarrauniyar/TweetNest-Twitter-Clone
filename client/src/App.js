cde7a3ac240279368fb53ac263ea6afed6456686
ajeetkumarrauniyar <ajeetkumarrauniyar@gmail.com>
2023-10-05 20:22:15 +0530

    Created Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+// import PostOverview from './pages/PostOverview';
+// import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-bg'>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/signup" element={<Signup />}></Route>
+          {/* <Route exact path="/posts" element={<PostOverview />}></Route>
+          <Route exact path="/myprofile" element={<Profile />}></Route> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
