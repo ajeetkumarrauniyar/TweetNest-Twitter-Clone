ae5fe39a03620a1ea4f27cca2aa8a45064dc300c
Ajeet Kumar <ajeetkumarrauniyar@gmail.com>
2023-12-01 10:26:03 +0530

    update

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -2,13 +2,18 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 
 import Login from "../pages/Auth/Login";
 import Register from "../pages/Auth/Register";
+import Home from "../pages/Home/Home";
+import Profile from "../pages/Profile/ProfileDetails";
+import Tweet from "../pages/Tweet/TweetDetails";
+import Navbar from "./Navbar/Navbar";
+import Error from "../pages/404/Error";
 
 export default function Router() {
   const Layout = () => {
     return (
       <div className="flex container h-screen w-full">
         <div className="lg:w-1/5 border-r border-lighter px-2 lg:px-6 py-2 flex flex-col justify-between">
-          <h1>Left</h1>
+          <Navbar />
         </div>
         <div className="w-full md:w-1/2 h-full overflow-y-scroll">
           <Outlet />
@@ -24,12 +29,14 @@ export default function Router() {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/auth/register" element={<Register />} />
+          <Route path="/auth/login" element={<Login />} />
           <Route path="/" element={<Layout />}>
-            <Route path="/" element={<>Home</>} />
-            <Route path="/profile/:id" element={<>ProfileDetailsPage</>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/profile/:id" element={<Profile />} />
+            <Route path="/tweet" element={<Tweet />} />
           </Route>
+          <Route path="*" element={<Error />} />
         </Routes>
       </BrowserRouter>
     );
